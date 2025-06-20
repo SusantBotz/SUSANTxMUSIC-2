f7a6ddac55682ff2faf3b482ccb2b502e1da3c0e
SusantBotz <susantbotz@gmail.com>
2025-06-21 00:20:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ OWNER_ID = int(getenv("OWNER_ID", 5909658683))
 
 
 # make your bots privacy from telegra.ph and put your url here 
-PRIVACY_LINK = getenv("PRIVACY_LINK", "https://graph.org/PRIVACY-FOR-TEAM-PURVI-BOTS-09-18")
+PRIVACY_LINK = getenv("PRIVACY_LINK", "https://github.com/SusantBotz/SUSANTxMUSIC")
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -44,15 +44,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/aniflixmusic/VillainMusicods",
+    "https://github.com/SusantBotz/SUSANTxMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SSTXMUSIC_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SST_COMMUNITYY")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SUSANTXBOTZ")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DragonByteGc")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -90,7 +90,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/iknaae.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/zspbkq.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/iknaae.jpg"
